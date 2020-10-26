1602846d6a608e2d0418dfa6335e3d8571443261
livezingy <ink.cheng@gmail.com>
2020-10-26 10:24:39 +0800

    Update tesstsv2map.h

--- tesstsv2map.h
+++ tesstsv2map.h
@@ -8,7 +8,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 
 std::vector<std::string> splitString(const std::string &str,const char* delimiter);
-std::map<std::string, std::vector<std::string>> tsv2map(cv::Mat tessImage, int magnifiRatio, const char* lang, const char* langPath, int psmVal);
+std::map<std::string, std::vector<std::string>> tsv2map(cv::Mat tessImage, int magnifiRatio, const char* lang, const char* langPath);
 
 
 #endif	
